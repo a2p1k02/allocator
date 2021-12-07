e6cca77cbfa234a5addf3b5db79f24dfd106f9ac
a2p1k02 <fl4ckyp@gmail.com>
2021-12-07 15:40:36 +0300

    Fix tabs Pog

--- src/allocator/allocator.c
+++ src/allocator/allocator.c
@@ -3,49 +3,49 @@
 
 size_t align(size_t n)
 {
-	return (n + sizeof(word_t) - 1) & ~(sizeof(word_t) - 1);
+    return (n + sizeof(word_t) - 1) & ~(sizeof(word_t) - 1);
 }
 
 size_t allocate_size(size_t size)
 {
-	return size + sizeof(Block) - sizeof(((struct Block*)0)->data);
+    return size + sizeof(Block) - sizeof(((struct Block*)0)->data);
 }
 
 Block* request_from_os(size_t size)
 {
-	Block* block = (Block* ) sbrk(0);
-	if (sbrk(allocate_size(size)) == (void *) - 1)
-		return NULL;
-	return block;
+    Block* block = (Block* ) sbrk(0);
+    if (sbrk(allocate_size(size)) == (void *) - 1)
+        return NULL;
+    return block;
 }
 
 word_t* allocate(size_t size)
 {
-	size = align(size);
+    size = align(size);
 
-	Block* block = request_from_os(size);
+    Block* block = request_from_os(size);
 
-	block->size = size;
-	block->used = true;
-	if (heap == NULL)
-		heap = block;
+    block->size = size;
+    block->used = true;
+    if (heap == NULL)
+        heap = block;
 
-	if (top != NULL)
-		top->next = block;
+    if (top != NULL)
+        top->next = block;
 
-	top = block;
+    top = block;
 
-	return block->data;
+    return block->data;
 }
 
 Block* get_header(word_t* data)
 {
-	return (Block *) ((char *) data + sizeof(((struct Block*)0)->data) - sizeof(Block));
+    return (Block *) ((char *) data + sizeof(((struct Block*)0)->data) - sizeof(Block));
 }
 
 void free_memory(word_t* data)
 {
-	Block* block = get_header(data);
-	block->used = false;
+    Block* block = get_header(data);
+    block->used = false;
 }
 
--- src/main.c
+++ src/main.c
@@ -5,16 +5,16 @@
 int main()
 {
     word_t* p1 = allocate(3);
-	Block* p1b = get_header(p1);
+    Block* p1b = get_header(p1);
 
-	assert(p1b->size == sizeof(word_t));
+    assert(p1b->size == sizeof(word_t));
 
-	word_t* p2 = allocate(8);
-	Block* p2b = get_header(p2);
+    word_t* p2 = allocate(8);
+    Block* p2b = get_header(p2);
 
-	assert(p2b->size == 8);
+    assert(p2b->size == 8);
 
-	fprintf(stderr, "\nAll assertions passed!\n");
+    fprintf(stderr, "\nAll assertions passed!\n");
 
     return 0;
 }
